c2f8c2d43f4e2356dc837ecbe24f397e2f9ffa87
Pierre Dahrouj <pdahrouj7@gmail.com>
2025-08-04 14:31:15 -0700

    Fix CORS to allow frontend origins

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "https://gray-ground-06620cc0f.1.azurestaticapps.net")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
